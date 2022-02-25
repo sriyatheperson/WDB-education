0bc67e68ef9c2607e48ccd3107a57a09701666aa
Sriya Kappagantu <sriya.kappagantu@berkeley.edu>
2022-02-24 21:25:04 -0800

    project

---
+++ homework1/location.js
@@ -0,0 +1,7 @@
+function printHome(city, state, country){
+    console.log(`I am from ${city}, ${state}, ${country}`);
+}
+var city = 'Cerritos';
+var state = 'CA';
+var country = 'USA';
+printHome(city, state, country);
